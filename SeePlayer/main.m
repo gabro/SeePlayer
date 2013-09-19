f425c4dd1507bcb9bdc768dac6cd9394c111bac0
Gabriele Petronella <gabriele.petronella@gmail.com>
2013-09-19 10:54:20 -0500

    Initial Commit

---
+++ SeePlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SeePlayer
+//
+//  Created by Gabriele Petronella on 9/19/13.
+//  Copyright (c) 2013 buildo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
---
+++ SeePlayerTests/SeePlayerTests.m
@@ -0,0 +1,34 @@
+//
+//  SeePlayerTests.m
+//  SeePlayerTests
+//
+//  Created by Gabriele Petronella on 9/19/13.
+//  Copyright (c) 2013 buildo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SeePlayerTests : XCTestCase
+
+@end
+
+@implementation SeePlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
